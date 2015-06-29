64519eb0f6681df7e1a953ac7b3bb1cd30d3e6e3
Jarod Reyes <jarodreyes@gmail.com>
2015-06-29 14:10:12 -0700

    Remove dead verify routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get "users/verify", to: 'users#show_verify', as: 'verify'
-  post "users/verify"
-  post "users/resend"
-
   get "login/", :to => "sessions#login"
   get "logout/", :to => "sessions#logout"
   post "login_attempt/", :to => "sessions#login_attempt"
