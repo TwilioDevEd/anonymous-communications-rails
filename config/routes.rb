3b08a6cde194243d3de0bb4933b9a81d1d579d26
Jarod Reyes <jarodreyes@gmail.com>
2015-07-30 11:31:52 -0700

    Tutorial for Anonymous Communications.

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
   post "login_attempt/", to: "sessions#login_attempt"
 
   resources :users, only: [:new, :create, :show]
-
   resources :vacation_properties, path: "/properties"
   resources :reservations, only: [:new, :create]
-  post "reservations/incoming", to: 'reservations#accept_or_reject', as: 'incoming'
 
+  # Twilio webhooks
   post "reservations/connect_sms", to: 'reservations#connect_guest_to_host_sms'
   post "reservations/connect_voice", to: 'reservations#connect_guest_to_host_voice'
+  post "reservations/incoming", to: 'reservations#accept_or_reject', as: 'incoming'
 
   # Home page
   root 'main#index', as: 'home'
